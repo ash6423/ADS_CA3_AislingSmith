963f8747a37b7384240415eb201f904f833088a1
D00223356 <d00223356@student.dkit.ie>
2021-12-12 16:15:23 +0000

    Created a struct for the binary tree nodes, then was able to create a full balanced binary tree using pointers to fill the nodes from left to right.

--- CA3_AislingSmith/binaryTree.h
+++ CA3_AislingSmith/binaryTree.h
@@ -5,18 +5,19 @@ using namespace std;
 
 //Using class notes from starter code and youtube videos I was able to create a struct of a node and a balanced binary tree, youtube video links are, https://www.youtube.com/watch?v=i0t63WWkfow
 
-
-template <class T>
 //Binary tree node 
 struct Node {
-	T data;
-	Node *left; //pointer to left hand side of the tree
-	Node *right;  //pointer to right hand side of the tree
+	int key;
+	struct Node* left; //pointer to left hand side of the tree
+	struct Node* right;  //pointer to right hand side of the tree
+
+	Node(int value) {
+		key = value; //the varible value is the key in the binary tree
 	
-	Node(T value) {
-		data = value;
-		//left = right = NULL;
+		left = NULL;
+		right = NULL;
 	}
+
 };
 
 template <class T>
@@ -30,6 +31,20 @@ public:
 	~BinaryTree();
 };
 
+int main() {
+	struct Node* root = new Node(1); //creating root node on the binary tree 
+
+	//creating branches(children) to the left and right of the root node.
+	root->left = new Node(2);
+	root->right = new Node(3);
+
+	root->left->left = new Node(4); //creating the first node on the lower left side of the tree.
+	root->left->right = new Node(5); //creating the node on the left side of the tree to the right.
+
+	root->right->left = new Node(6); //creating the first node on the lower side of the right handside of the tree
+	root->right->right = new Node(7); //creating the last node of a balanced binary tree, on the lower right handside of the tree.
+
+};
 
 
 
